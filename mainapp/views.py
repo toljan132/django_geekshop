2c9e154dfb62ed4052e14798d63723d08f73b281
toljan132 <toljan132@gmail.com>
2019-04-02 14:31:14 +0300

    Урок 3. Модели + ORM = данные

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
+from .models import ProductCategory
 
 
 def main(request):
     return render(request, 'mainapp/index.html')
     
-
-def products(request):
-    return render(request, 'mainapp/products.html')
+def products(request, pk=None):
+    categories_list = ProductCategory.objects.all()
+    return render(request, 'mainapp/products.html', context= {'categories': categories_list })
 
 def contacts(request):
     return render(request, 'mainapp/contacts.html')
